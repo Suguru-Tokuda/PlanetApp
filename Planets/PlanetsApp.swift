beb305e89d0d0fe235c4b0f3f9f7acbcb3e7f44e
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-20 21:54:34 -0500

    Initial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Suguru Tokuda on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
